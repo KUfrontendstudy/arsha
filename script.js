ef1969151cfd9b0ce855e91451e33069a93e96b2
Yongjun Park <yopark@student.42seoul.kr>
2023-04-07 00:50:29 +0900

    feat: :sparkles: accordian 기능 초기 구현

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const accordions = document.querySelectorAll('.why-us__item__header');
+
+// FIXME: 동시에 변하질 않음
+accordions.forEach((accordion) =>
+  accordion.addEventListener('click', (e) => {
+    accordions.forEach((accordion) => {
+      if (accordion === e.currentTarget) {
+        e.currentTarget.nextElementSibling.classList.toggle('active');
+        e.currentTarget.querySelector('.chevron-up').classList.toggle('hidden');
+        e.currentTarget
+          .querySelector('.chevron-down')
+          .classList.toggle('hidden');
+        return;
+      }
+      if (accordion.nextElementSibling.classList.contains('active')) {
+        accordion.nextElementSibling.classList.remove('active');
+        accordion.querySelector('.chevron-up').classList.add('hidden');
+        accordion.querySelector('.chevron-down').classList.remove('hidden');
+      }
+    });
+  })
+);
